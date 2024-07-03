72745117c5c465ea6161d826d65fb2496e990ed4
ondghoskot <113732832+ondghoskot@users.noreply.github.com>
2024-07-03 21:08:08 +0100

    chore: GO BACK

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,27 +1,20 @@
 const Token = require("../models/token");
-const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = async (req, res, next) => {
     try {
         const token = req.header("Authorization").replace("Bearer ", "");
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-
-        if (!user) {
-            throw new Error();
-        }
+        const verify = jwt.verify(token, "secretkey");
+        const findToken = await Token.findOne({ token, userId: verify.userId});
         
-        if (!decoded) {
+        if (!findToken) {
             throw new Error("Invalid token");
         }
 
-        if (decoded.expiredAt < Date.now()) {
+        if (findToken.expiredAt < Date.now()) {
             throw new Error("Token expired");
         }
-
-        req.token = token;
-        req.user = user;
+        req.user = verify;
         next();
     } catch (error) {
         res.status(401).send(error.message);
