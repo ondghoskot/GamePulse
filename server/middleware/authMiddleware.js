425f7e1b661b85032f78627331763077efa5439d
ondghoskot <113732832+ondghoskot@users.noreply.github.com>
2024-07-03 20:59:36 +0100

    fix: recognize user._id

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,20 +1,27 @@
 const Token = require("../models/token");
+const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = async (req, res, next) => {
     try {
         const token = req.header("Authorization").replace("Bearer ", "");
-        const verify = jwt.verify(token, "secretkey");
-        const findToken = await Token.findOne({ token, userId: verify.userId});
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if (!user) {
+            throw new Error();
+        }
         
-        if (!findToken) {
+        if (!decoded) {
             throw new Error("Invalid token");
         }
 
-        if (findToken.expiredAt < Date.now()) {
+        if (decoded.expiredAt < Date.now()) {
             throw new Error("Token expired");
         }
-        req.user = verify;
+
+        req.token = token;
+        req.user = user;
         next();
     } catch (error) {
         res.status(401).send(error.message);
