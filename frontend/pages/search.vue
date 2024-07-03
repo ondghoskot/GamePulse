5339ef35d2e16f0750d0f8f3da9e019ff0219380
ondghoskot <113732832+ondghoskot@users.noreply.github.com>
2024-07-03 21:18:39 +0100

    chore: testing search

--- frontend/pages/search.vue
+++ frontend/pages/search.vue
@@ -27,38 +27,14 @@ export default {
   },
   async asyncData({ $axios, query }) {
     if (query?.q) {
-      let searchResults = [
-        {
-          title: "Hollow Knight",
-          rating: 4.1,
-          releaseDate: "2018-09-12",
-          img: "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/367520/capsule_616x353.jpg?t=1695270428",
-          summary: "Lorem Ipsum is simply dummy text of the printing .",
-        },
-        {
-          title: "Terraria",
-          rating: 4.8,
-          releaseDate: "2015-03-25",
-          img: "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000001702/dac3a26570b5ca1ddf703bf0add7cc7c527f71a2b56521baf69e20c7a573c610",
-          summary:
-            "Lorem Ipsum is simply dummy prot only five centuries, but also g and typesetting industry.g and typesetting industry.g and typesetting industry. of Lorem Ipsum.",
-        },
-        {
-          title: "Super Meat Boy",
-          rating: 3.3,
-          releaseDate: "2022-11-09",
-          img: "https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.5/ncom/software/switch/70010000001990/cd98a2abfb3daabf755ee6230142a01cf1b2f717d8831093545595280539f1eb",
-          summary:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's Ipsum.",
-        },
-      ];
+      let searchResults = [];
 
-      //   try {
-      //     let results = $axios.$get("/games/search?keyword=" + query.q);
-      //     searchResults = results.data;
-      //   } catch (err) {
-      //     console.log(err);
-      //   }
+         try {
+           let results = $axios.get("/games/results?keyword=" + query.q);
+           searchResults = results.data;
+         } catch (err) {
+           console.log(err);
+         }
       return { searchResults, ...{ query: query.q } };
     }
   },
